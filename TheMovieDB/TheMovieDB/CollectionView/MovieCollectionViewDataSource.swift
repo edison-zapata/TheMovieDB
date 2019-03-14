0286c5fcfa38f1d0f694d2aaec357450c4642388
Edison Zapata Henao <edison.zapata@globant.com>
2019-03-14 16:48:16 -0500

    Added FlowLayout personalization to CollectionView

---
+++ TheMovieDB/TheMovieDB/CollectionView/MovieCollectionViewDataSource.swift
@@ -0,0 +1,60 @@
+//
+//  MovieCollectionViewDelegate.swift
+//  TheMovieDB
+//
+//  Created by Edison Zapata Henao on 3/14/19.
+//  Copyright © 2019 Globant. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MovieCollectionViewDelegate : UICollectionViewDelegate{
+    func isEqual(_ object: Any?) -> Bool {
+        <#code#>
+    }
+    
+    var hash: Int = 0
+    
+    var superclass: AnyClass?
+    
+    func `self`() -> Self {
+        <#code#>
+    }
+    
+    func perform(_ aSelector: Selector!) -> Unmanaged<AnyObject>! {
+        <#code#>
+    }
+    
+    func perform(_ aSelector: Selector!, with object: Any!) -> Unmanaged<AnyObject>! {
+        <#code#>
+    }
+    
+    func perform(_ aSelector: Selector!, with object1: Any!, with object2: Any!) -> Unmanaged<AnyObject>! {
+        <#code#>
+    }
+    
+    func isProxy() -> Bool {
+        <#code#>
+    }
+    
+    func isKind(of aClass: AnyClass) -> Bool {
+        <#code#>
+    }
+    
+    func isMember(of aClass: AnyClass) -> Bool {
+        <#code#>
+    }
+    
+    func conforms(to aProtocol: Protocol) -> Bool {
+        <#code#>
+    }
+    
+    func responds(to aSelector: Selector!) -> Bool {
+        <#code#>
+    }
+    
+    var description: String = ""
+    
+    
+}
